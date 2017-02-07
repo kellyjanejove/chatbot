bb111e644147e7ae2883df15fff04f17164041c8
DIR\anna.trivino <anna.trivino@accenture.com>
2017-02-06 20:16:54 -0600

    "Promisified" lambdas under Assignee

--- Sprint 2/PMG.Services/Assignee/get-assignee-names.js
+++ Sprint 2/PMG.Services/Assignee/get-assignee-names.js
@@ -1,47 +1,30 @@
 'use strict';
 
 const sql = require('mssql');
-const AWS = require('aws-sdk');
-AWS.config.region = 'us-east-1';
-const lambda = new AWS.Lambda();
+const utils = require('../utils/lambda-utils');
 
-console.log('Loading...');
+exports.handler = function(event, context, callback) {
 
-const params = {
-    FunctionName: '6900-PMG-Dev-Get-Dbconfig-From-S3'
-};
+    console.log('Loading...');
 
-exports.handler = function (event, context, callback) {
-    try {
-        lambda.invoke(params, function (err, data) {
-            if (err) {
-                handleError(err);
-            } else {
-                var connString = JSON.parse(data.Payload);
+    utils.getDbConfig()
+        .then(function(data) {
+            var connString = JSON.parse(data.Body);
 
-                sql.connect(connString, function (err) {
-                    if (err) {
-                        handleError(err);
-                    } else {
-                        getList(function (err, data) {
-                            sql.close();
-                            if (err) {
-                                handleError(err);
-                            } else {
-                                callback(null, {
-                                    statusCode: 200,
-                                    headers: {},
-                                    body: JSON.stringify(data)
-                                });
-                            }
-                        });
-                    }
+            sql.connect(connString)
+                .then(getList)
+                .then(function(data) {
+                    sql.close();
+                    callback(null, {
+                        statusCode: 200,
+                        body: JSON.stringify(data)
+                    });
+                }).catch(function(err) {
+                    utils.handleError(err, callback);
                 });
-            }
+        }).catch(function(err) {
+            utils.handleError(err, callback);
         });
-    } catch (err) {
-        handleError(err);
-    }
 
     function getList(callback) {
         var request = new sql.Request();
@@ -58,22 +41,12 @@ exports.handler = function (event, context, callback) {
         }
         query = query + ' ORDER BY [Full Name]';
 
-        request.query(query, function (error, data) {
-            callback(error, data);
-        });
+        return request.query(query);
     }
 
     function getQuery() {
         return `SELECT TOP 10 (LTRIM(RTRIM([LastNm])) + ', ' + LTRIM(RTRIM([FirstNm])) + ' ' +
                     LTRIM(RTRIM([MiddleNm])) + ' - ' + LTRIM(RTRIM([AssigneePersonnelNbr]))) AS [Full Name]
                 FROM Assignee WITH(NOLOCK) `;
-    }
-
-    function handleError(err) {
-        console.log(err.message);
-        callback(null, {
-            statusCode: 500,
-            body: err.message
-        });
     }
 };
