d13b670a268f4ea369b1b2168eb27e5fde201b06
DIR\anna.trivino <anna.trivino@accenture.com>
2017-02-17 15:36:27 -0600

    Breakdown/Undo breakdown;  Search changes

---
+++ Sprint 2/PMG.Services/JournalTransaction/post-journal-breakdown.js
@@ -0,0 +1,229 @@
+'use strict';
+
+const sql = require('mssql');
+const utils = require('../utils/lambda-utils');
+
+exports.handler = function (event, context, callback) {
+
+    console.log('Loading...');
+
+    if (!event.body) {
+        callback(null, {
+            statusCode: 400,
+            body: 'Bad request'
+        });
+    } else {
+        var body = JSON.parse(event.body);
+
+        utils.getDbConfig()
+            .then(function (data) {
+                var connString = JSON.parse(data.Body);
+
+                sql.connect(connString, function (err, result) {
+                    if (err) {
+                        utils.handleError(err);
+                    } else {
+                        createChildren(body);
+                    }
+                });
+            });
+    };
+
+    function createChildren(records) {
+        createChild(0, records);
+    }
+
+    function createChild(index, records) {
+        if (index === records.length) {
+            //res.json();
+            updateParent(records[0]);
+        } else {
+            let breakdownParam = JSON.parse(records[index]);
+            getCreateQuery(breakdownParam);
+            createChild(index + 1, records);
+        }
+    }
+
+    function getCreateQuery(breakdownParam) {
+        var request = new sql.Request();
+        var query = `
+            BEGIN TRY
+                BEGIN TRAN`;
+
+        request.input('journalTransactionId', sql.Int, breakdownParam.JournalTransactionId);
+        request.input('mappingStatusInd', sql.VarChar, breakdownParam.MappingStatusInd);
+        request.input('assigneePersonnelNbr', sql.VarChar, breakdownParam.AssigneePersonnelNbr);
+        request.input('taxYearNbr', sql.VarChar, breakdownParam.TaxYearNbr);
+        request.input('journalTransactionTypeId', sql.Int, breakdownParam.JournalTransactionTypeId);
+        request.input('journalTransactionTypeDesc', sql.VarChar, breakdownParam.JournalTransactionTypeDesc);
+        request.input('clientCd', sql.VarChar, breakdownParam.ClientCd);
+        request.input('clientNm', sql.VarChar, breakdownParam.ClientNm);
+        request.input('localCurrencyAmt', sql.Decimal(24, 4), breakdownParam.localCurrencyAmt);
+        request.input('usDollarAmt', sql.Decimal(24, 4), breakdownParam.USDollarAmt);
+        request.input('commentsTxt', sql.VarChar, breakdownParam.CommentsTxt);
+        request.input('updateUserId', sql.VarChar, breakdownParam.UpdateUserId);
+        request.input('jDParentId', sql.Int, breakdownParam.jDParentId);
+        request.input('isDeletedInd', sql.VarChar, breakdownParam.isDeletedInd);
+        request.input('TravelPlanIndicator', sql.Int, breakdownParam.TravelPlanIndicator);
+
+        query = query + `
+                    INSERT INTO JournalTransactionDetail(JournalTransactionId
+                                                        ,SplitInd
+                                                        ,MappingStatusInd
+                                                        ,AssigneePersonnelNbr
+                                                        ,TaxYearNbr
+                                                        ,JournalTransactionTypeId
+                                                        ,JournalTransactionTypeDesc
+                                                        ,ClientCd
+                                                        ,ClientNm
+                                                        ,LocalCurrencyAmt
+                                                        ,USDollarAmt
+                                                        ,CommentsTxt
+                                                        ,CreateUserId
+                                                        ,CreateDttm
+                                                        ,UpdateUserId
+                                                        ,UpdateDttm
+                                                        ,JDParentId
+                                                        ,IsDeletedInd
+                                                        ,TravelPlanIndicator)`;
+        query = query + `
+                    VALUES(@journalTransactionId
+                        ,'N'
+                        ,@mappingStatusInd
+                        ,@assigneePersonnelNbr
+                        ,@taxYearNbr
+                        ,@journalTransactionTypeId
+                        ,@journalTransactionTypeDesc
+                        ,@clientCd
+                        ,@clientNm
+                        ,@localCurrencyAmt
+                        ,@usDollarAmt
+                        ,@commentsTxt
+                        ,@updateUserId
+                        ,SYSDATETIME()
+                        ,@updateUserId
+                        ,SYSDATETIME()
+                        ,@jDParentId
+                        ,@isDeletedInd
+                        ,@TravelPlanIndicator)`;
+        query = query + `
+                COMMIT TRAN
+            END TRY
+            BEGIN CATCH
+                ROLLBACK TRAN
+            END CATCH`;
+
+        request.query(query, function (error, data) {
+            if (error) {
+                console.log(error);
+            }
+        });
+    }
+
+    function updateParent(records) {
+        let breakdownParam = JSON.parse(records);
+
+        getParentQuery(breakdownParam);
+
+        //res.json();
+    }
+
+    function getParentQuery(breakdownParam) {
+        var request = new sql.Request();
+        var query = `
+            BEGIN TRY
+                BEGIN TRAN`;
+
+        request.input('JournalTransactionDetailId', sql.Int, breakdownParam.jDParentId);
+        request.input('updateUserId', sql.VarChar, breakdownParam.UpdateUserId);
+
+        query = query + `
+                    UPDATE JournalTransactionDetail
+                    SET SplitInd = 'Y',
+                    UpdateUserId = @updateUserId,
+                    UpdateDttm = SYSDATETIME()
+                    WHERE JournalTransactionDetailId = @JournalTransactionDetailId
+                COMMIT TRAN
+            END TRY
+            BEGIN CATCH
+                ROLLBACK TRAN
+            END CATCH`;
+
+        request.query(query, function (error, data) {
+            if (error) {
+                console.log(error);
+            } else {
+                //auditChildren(breakdownParam.jDParentId);  
+            }
+        });
+    }
+
+    function auditChildren(jDParentId) {
+        var request = new sql.Request();
+        request.input('jDParentId', sql.Int, jDParentId);
+
+        var query = `BEGIN TRY
+                        BEGIN TRAN
+                        INSERT INTO JournalTransactionDetailVersion (
+                                JournalTransactionDetailId
+                                ,JournalTransactionId
+                                ,SplitInd
+                                ,MappingStatusInd
+                                ,AssigneePersonnelNbr
+                                ,TaxYearNbr
+                                ,JournalTransactionTypeId
+                                ,JournalTransactionTypeDesc
+                                ,ClientCd
+                                ,ClientNm
+                                ,LocalCurrencyAmt
+                                ,USDollarAmt
+                                ,CommentsTxt
+                                ,JDParentId
+                                ,IsDeletedInd
+                                ,TravelPlanIndicator
+                                ,CreateUserId
+                                ,CreateDttm
+                                ,UpdateUserId
+                                ,UpdateDttm
+                        )
+                        SELECT  JournalTransactionDetailId
+                                ,JournalTransactionId
+                                ,SplitInd
+                                ,MappingStatusInd
+                                ,AssigneePersonnelNbr
+                                ,TaxYearNbr
+                                ,JournalTransactionTypeId
+                                ,JournalTransactionTypeDesc
+                                ,ClientCd
+                                ,ClientNm
+                                ,LocalCurrencyAmt
+                                ,USDollarAmt
+                                ,CommentsTxt
+                                ,JDParentId
+                                ,IsDeletedInd
+                                ,TravelPlanIndicator
+                                ,CreateUserId
+                                ,CreateDttm
+                                ,UpdateUserId
+                                ,UpdateDttm  
+                        FROM JournalTransactionDetail 
+                        WHERE ISNULL(IsDeletedInd,'N') <> 'Y'
+                                AND (JDParentId = @jDParentId
+                                        OR JournalTransactionDetailId = @jDParentId)
+                    COMMIT TRAN
+                END TRY
+                    
+                BEGIN CATCH
+                    ROLLBACK TRAN    
+                END CATCH`;
+
+        request.query(query, function (error, data) {
+            if (error) {
+                console.log(error);
+            } else {
+                //res.json();
+            }
+        });
+    }
+};
+
---
+++ Sprint 2/PMG.Services/JournalTransaction/put-journal-breakdown.js
@@ -0,0 +1,144 @@
+'use strict';
+
+const sql = require('mssql');
+const utils = require('../utils/lambda-utils');
+
+exports.handler = function (event, context, callback) {
+
+    console.log('Loading...');
+
+    if (!event.body) {
+        callback(null, {
+            statusCode: 400,
+            body: 'Bad request'
+        });
+    } else {
+        var body = JSON.parse(event.body);
+
+        utils.getDbConfig()
+            .then(function (data) {
+                var connString = JSON.parse(data.Body);
+
+                sql.connect(connString, function (err, result) {
+                    if (err) {
+                        utils.handleError(err);
+                    } else {
+                        undoChild(body);
+                    }
+                });
+            });
+    };
+
+    function undoChild(breakdownParam) {
+        var request = new sql.Request();
+
+        //request.input('journalTransactionDetailID', sql.VarChar, updateParam.JournalTransactionDetailID);
+        request.input('jDParentId', sql.Int, breakdownParam[0]);
+        request.input('updateUserId', sql.VarChar, breakdownParam[1]);
+
+        var query = `   DECLARE @temp table (JournalTransactionDetailId int);
+
+                        INSERT INTO @temp
+                        SELECT jd.JournalTransactionDetailId
+                        FROM JournalTransactionDetail jd WITH(NOLOCK)
+                        WHERE jd.JdParentId = @jDParentId
+
+                        BEGIN TRY
+                            BEGIN TRAN
+                                UPDATE jd
+                                SET isDeletedInd = 'Y',
+                                UpdateUserId = @updateUserId,
+                                UpdateDttm = SYSDATETIME()
+                                FROM JournalTransactionDetail jd
+                                JOIN @temp t ON t.JournalTransactionDetailId = jd.JournalTransactionDetailId
+
+                                UPDATE JournalTransactionDetail
+                                SET SplitInd = 'N',
+                                UpdateUserId = @updateUserId,
+                                UpdateDttm = SYSDATETIME()
+                                WHERE JournalTransactionDetailId = @jDParentId
+
+                                INSERT INTO JournalTransactionDetailVersion (
+                                        JournalTransactionDetailId
+                                        ,JournalTransactionId
+                                        ,SplitInd
+                                        ,MappingStatusInd
+                                        ,AssigneePersonnelNbr
+                                        ,TaxYearNbr
+                                        ,JournalTransactionTypeId
+                                        ,JournalTransactionTypeDesc
+                                        ,ClientCd
+                                        ,ClientNm
+                                        ,LocalCurrencyAmt
+                                        ,USDollarAmt
+                                        ,CommentsTxt
+                                        ,JDParentId
+                                        ,IsDeletedInd
+                                        ,TravelPlanIndicator
+                                        ,CreateUserId
+                                        ,CreateDttm
+                                        ,UpdateUserId
+                                        ,UpdateDttm
+                                )
+                                SELECT  JournalTransactionDetailId
+                                        ,JournalTransactionId
+                                        ,SplitInd
+                                        ,MappingStatusInd
+                                        ,AssigneePersonnelNbr
+                                        ,TaxYearNbr
+                                        ,JournalTransactionTypeId
+                                        ,JournalTransactionTypeDesc
+                                        ,ClientCd
+                                        ,ClientNm
+                                        ,LocalCurrencyAmt
+                                        ,USDollarAmt
+                                        ,CommentsTxt
+                                        ,JDParentId
+                                        ,IsDeletedInd
+                                        ,TravelPlanIndicator
+                                        ,CreateUserId
+                                        ,CreateDttm
+                                        ,UpdateUserId
+                                        ,UpdateDttm 
+                                FROM JournalTransactionDetail 
+                                WHERE JournalTransactionDetailId IN (SELECT JournalTransactionDetailId
+                                                                        FROM @temp)
+                                    OR JournalTransactionDetailId = @jDParentId
+
+                            COMMIT TRAN
+                        END TRY
+                        BEGIN CATCH
+                            ROLLBACK TRAN
+                        END CATCH `;
+        request.query(query, function (error, data) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json();
+            }
+        });
+    }
+
+    function undoParentSplit(breakdownParam) {
+        var request = new sql.Request();
+        request.input('jDParentId', sql.int, breakdownParam.jDParentId);
+        
+        var query = `   BEGIN TRY
+                            BEGIN TRAN
+                                UPDATE JournalTransactionDetail
+                                SET SplitInd = 'N'
+                                WHERE JournalTransactionDetailId = @jDParentId
+                            COMMIT TRAN
+                        END TRY
+                        BEGIN CATCH
+                            ROLLBACK TRAN
+                        END CATCH `;
+
+        request.query(query, function (error, data) {
+            if (error) {
+                console.log(error);
+            }
+        });
+    }
+
+};
