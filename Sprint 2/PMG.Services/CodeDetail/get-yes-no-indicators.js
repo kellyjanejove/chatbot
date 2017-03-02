914c5fab133ae6251d4e43242c3a3a6df6be7816
DIR\anna.trivino <anna.trivino@accenture.com>
2017-03-02 13:52:38 -0600

    Added Yes No Indicator

---
+++ Sprint 2/PMG.Services/CodeDetail/get-yes-no-indicators.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const sql = require('mssql');
+const utils = require('../utils/lambda-utils');
+
+exports.handler = function (event, context, callback) {
+    console.log('Loading...');
+
+    utils.getDbConfig()
+        .then(function (data) {
+            var connString = JSON.parse(data.Body);
+
+            sql.connect(connString)
+                .then(getList)
+                .then(function (data) {
+                    sql.close();
+                    callback(null, {
+                        statusCode: 200,
+                        headers: {
+                            'Access-Control-Allow-Origin': '*' // Required for CORS support to work
+                        },
+                        body: JSON.stringify(data)
+                    });
+                }).catch(function (err) {
+                    utils.handleError(err, callback);
+                });
+        }).catch(function (err) {
+            utils.handleError(err, callback);
+        });
+
+    function getList() {
+        var request = new sql.Request();
+        var query = getQuery();
+
+        return request.query(query);
+    }
+
+    function getQuery() {
+        return `SELECT [DecodeTxt]
+                FROM CodeDetail WITH(NOLOCK)
+                WHERE [CategoryNbr] = 24`;
+    }
+};
